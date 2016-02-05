cd2bb689aa9c4866da0493577a6568ac218e0102
Greg Ziebold <ziebold@gmail.com>
2016-02-05 11:32:05 -0700

    Insert support for react-intl
    * any components using react-intl will require IntlProvider wrapper

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import Container from '../Container'
 import Header from '../Header'
 import Main from '../Main'
+import { IntlProvider } from 'react-intl'
 
 export default class App extends Component {
   static displayName = 'SG.App'
@@ -14,7 +15,9 @@ export default class App extends Component {
     return (
       <Container>
         <Header ctx={this.props.ctx} />
-        <Main ctx={this.props.ctx} />
+        <IntlProvider>
+          <Main ctx={this.props.ctx} />
+        </IntlProvider>
       </Container>
     )
   }
